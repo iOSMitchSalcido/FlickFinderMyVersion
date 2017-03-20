b6262a20d93b20ec8a2253ca3cde9c2e59ec1d7f
iOSMitchSalcido <iosmitchsalcido@gmail.com>
2017-03-20 08:54:50 -0700

    experimenting with layout

--- FlickFinderMyVersion/LocationsViewController.swift
+++ FlickFinderMyVersion/LocationsViewController.swift
@@ -11,6 +11,8 @@ import MapKit
 
 class LocationsViewController: UIViewController {
 
+    let locations = ["San Francisco", "Chico, CA", "Los Angeles", "Moscow", "London", "Tokyo"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -24,4 +26,26 @@ class LocationsViewController: UIViewController {
     func dismissVC() {
         dismiss(animated: true, completion: nil)
     }
+}
+
+extension LocationsViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Popular Locations"
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return locations.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LocationsCellID")!
+        cell.textLabel?.text = locations[indexPath.row]
+        return cell
+    }
+}
+
+extension LocationsViewController: UITextFieldDelegate {
+    
 }
