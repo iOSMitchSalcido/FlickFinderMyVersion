3bf6feff4ff2d86497b4a24e8bd0ff76ab0b9235
iOSMitchSalcido <iosmitchsalcido@gmail.com>
2017-03-20 08:18:45 -0700

    outlets/actions good. Locations VC shows map

--- FlickFinderMyVersion/ViewController.swift
+++ FlickFinderMyVersion/ViewController.swift
@@ -10,16 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var appTitleLabel: UILabel!
+    @IBOutlet weak var flickTitleLabel: UILabel!
+    @IBOutlet weak var flickImageView: UIImageView!
+    @IBOutlet weak var phraseTextField: UITextField!
+    @IBOutlet weak var longitudeTextField: UITextField!
+    @IBOutlet weak var latitudeTextField: UITextField!
+    @IBOutlet weak var phraseSearchButton: UIButton!
+    @IBOutlet weak var locationsButton: UIButton!
+    @IBOutlet weak var geoSearchButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func phraseSearchButtonPressed(_ sender: UIButton) {
     }
-
-
+    
+    @IBAction func geoSearchButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func locationsButtonPressed(_ sender: UIButton) {
+        
+        let controller = storyboard?.instantiateViewController(withIdentifier: "LocationsViewController") as! LocationsViewController
+        let nc = UINavigationController(rootViewController: controller)
+        present(nc, animated: true, completion: nil)
+    }
 }
 
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
